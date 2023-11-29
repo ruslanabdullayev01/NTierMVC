05848c54188937dcb8894fead2f53fa544e7e9ce
ruslanabdullayev01 <ruslanabdullayev01@gmail.com>
2023-11-29 14:23:23 +0400

    Added Repositories and UnitOfWork

---
+++ DataAccess/Repositories/Concrete/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Common.Entities;
+using DataAccess.Context;
+using DataAccess.Repositories.Abstract;
+using DataAccess.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Concrete
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext db) : base(db)
+        {
+
+        }
+    }
+}
